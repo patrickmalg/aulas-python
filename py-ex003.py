e1b462d9f2224320e071b0bc9c26db11fa1da98c
patrickmalg <patrickmoreira2001@hotmail.com>
2019-02-19 13:35:31 -0300

    alo

---
+++ py-ex003.py
@@ -0,0 +1,19 @@
+print("TESTE")
+
+n = int(input("Digite um número: "))
+a = n - 1
+s = n + 1
+print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}'.format(n, a, s))
+print("FIM TESTE")
+
+print("===================================")
+
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+n3 = int(input("Digite um terceiro numero: "))
+tot = n1 + n2
+tot2 = tot + n3
+ant = tot2 - 1
+suce = tot2 + 1
+print("A soma do numeros é: ", tot2)
+print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}'.format(tot2, ant, suce))
---
+++ py-ex004.py
@@ -0,0 +1,9 @@
+n = int(input("Digite um número: "))
+dobro = n * 2
+triplo = n * 3
+raiz = n ** (1/2)
+metade = n / 2
+print('O dobro de {} vale {}.'.format(n, dobro))
+print('O triplo de {} vale {}.'.format(n, triplo)) 
+print('A raiz de {} vale {:.2f}.'.format(n, raiz))
+print('E a metade de {} vale {}.'.format(n, metade))
