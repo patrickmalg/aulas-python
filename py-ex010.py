e4aaefcedb0f6b9b67fd5962199ee4b7eee92a46
patrickmalg <patrickmoreira2001@hotmail.com>
2019-02-20 14:34:37 -0300

    sad

--- py-ex010.py
+++ py-ex010.py
@@ -1,3 +1,3 @@
 preco = float(input("Qual o preço do produto? R$"))
 novo = preco - (preco * 5 / 100)
-print('O produto que custava R${}, na promoção com desconto de 5% passou a custar R${}'.format(preco, novo))
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% passou a custar R${:.2f}'.format(preco, novo))
