6ebfb3beda98518bcab64d75c3979ad7ab10f2ce
patrickmalg <patrickmoreira2001@hotmail.com>
2019-02-22 13:29:26 -0300

    asas

---
+++ py-ex014.py
@@ -0,0 +1,4 @@
+import math
+num = int(input("Digite um numero para ver sua raiz quadrada: "))
+raiz = math.sqrt(num)
+print('A raiz quadrada de {} é {}'.format(num, raiz))
---
+++ py-ex015.py
@@ -0,0 +1,7 @@
+import math
+num = float(input("Digite um valor: "))
+print('O valor digitado foi {} e sua parte inteira é {}'.format(num, math.trunc(num)))
+
+print('-'*58)
+
+#OUTRA FORMA 
